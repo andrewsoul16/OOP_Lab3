c8c4ea85851f78bc532176b79958fb8eb25fda2f
Andrewsoul16 <kostromskoiandrei16@gmail.com>
2020-12-16 13:59:06 +0300

    code

---
+++ OOP_Lab3/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace OOP_Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.tbSorted = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(12, 120);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(133, 34);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Считать даные";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(166, 120);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(133, 34);
+            this.btnSort.TabIndex = 1;
+            this.btnSort.Text = "Сортировка";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(320, 120);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(133, 34);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Исходные данные";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Отсортированные данные";
+            // 
+            // tbSource
+            // 
+            this.tbSource.Location = new System.Drawing.Point(158, 22);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.ReadOnly = true;
+            this.tbSource.Size = new System.Drawing.Size(295, 20);
+            this.tbSource.TabIndex = 5;
+            // 
+            // tbSorted
+            // 
+            this.tbSorted.Location = new System.Drawing.Point(158, 56);
+            this.tbSorted.Name = "tbSorted";
+            this.tbSorted.ReadOnly = true;
+            this.tbSorted.Size = new System.Drawing.Size(295, 20);
+            this.tbSorted.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 207);
+            this.Controls.Add(this.tbSorted);
+            this.Controls.Add(this.tbSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.TextBox tbSorted;
+    }
+}
+
---
+++ OOP_Lab3/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OOP_Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] ints;
+
+        public void Read()
+        {
+            try
+            {
+                StreamReader reader = new StreamReader("source.txt");
+
+                String source = reader.ReadLine();
+
+                reader.Close();
+
+                ints = source.Split(' ').Select(Int32.Parse).ToArray();
+
+                for(int i = 0; i < 15; i++)
+                {
+                    tbSource.Text += ints[i];
+                    tbSource.Text += " ";
+                }                
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Exception: "+e.Message);
+            }
+        }
+
+        public void InsertionSort()
+        {
+            for (int i = 1; i < 15; i++)
+            {
+                int temp = ints[i];
+
+                if (ints[i]>ints[i-1])
+                {
+                    continue;
+                }
+                else
+                {
+                    int j = i;
+
+                    while (temp<=ints[j-1])
+                    {
+                        ints[j] = ints[j - 1];
+                        
+                        j--;
+
+                        if (j == 0) break;
+                    }
+
+                    ints[j] = temp;
+                }               
+            }
+        }
+
+        public void SelectionSort()
+        {
+            for (int i = 0; i < 14; i++)
+            {
+                int min = i;
+
+                for (int j = i + 1; j < 15; j++)
+                    {
+                    if (ints[j] < ints[min])
+                    {
+                        min = j;        
+                    }
+                }
+                int dummy = ints[i];
+                ints[i] = ints[min];
+                ints[min] = dummy;
+            }
+        }
+
+        public void BubbleSort()
+        {
+                int temp;
+                for (int i = 0; i < ints.Length - 1; i++)
+                {
+                    for (int j = 0; j < ints.Length - i - 1; j++)
+                    {
+                        if (ints[j + 1] < ints[j])
+                        {
+                            temp = ints[j + 1];
+                            ints[j + 1] = ints[j];
+                            ints[j] = temp;
+                        }
+                    }
+                }
+        }
+
+        public void ShellSort()
+        {
+            int step = ints.Length / 2;
+            while (step > 0)
+            {
+                int i, j;
+                for (i = step; i < ints.Length; i++)
+                {
+                    int value = ints[i];
+                    for (j = i - step; (j >= 0) && (ints[j] > value); j -= step)
+                        ints[j + step] = ints[j];
+                    ints[j + step] = value;
+                }
+                step /= 2;
+            }
+        }
+
+        public void QuickSort()
+        {
+            int pivot = ints.Length / 2;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Read();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tbSorted.Text != null)
+            {
+                tbSorted.Text = "";
+            }
+
+            //InsertionSort();
+            //SelectionSort();
+            //BubbleSort();
+            //ShellSort();
+            QuickSort();
+
+            for (int i = 0; i < 15; i++)
+            {
+                tbSorted.Text += ints[i];
+                tbSorted.Text += " ";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
